8f582274a8691d6efa70125c1951b087183b26f9
Ambler <dambler@igswzawgws-salsa.gs.doi.net>
2018-03-14 09:16:56 -0700

    lesson4

--- lesson4/l4e3.py
+++ lesson4/l4e3.py
@@ -1,12 +1,28 @@
 #!/usr/local/bin/python3
 from __future__ import unicode_literals, print_function
 from pprint import pprint
-
-
+import re
 
 with open("show_version.txt") as f:
-    output = f.read()
+    show_version  = f.read()
 
-test = output.replace('\n', '')
-match = re.search(r'Cisco.+?Version (?P<os_ver>\S.+?),.+board ID (?P<ser_num>\S.+?)\s.+register is (?P<conf_reg>\S.+)', test).groupdict()
-match.get('os_ver')
+## Find stuff with a one liner
+#test = output.replace('\n', '')
+#match = re.search(r'Cisco.+?Version (?P<os_ver>\S.+?),.+board ID (?P<ser_num>\S.+?)\s.+register is (?P<conf_reg>\S.+)', show_version).groupdict()
+#os_ver, ser_num, conf_reg = match.get('os_ver'), match.get('ser_num'), match.get('conf_reg')
+
+match = re.search(r'Cisco.+Version (\S.+),', show_version, flags=re.M)
+if match:
+    os_ver = match.group(1)
+
+match = re.search(r'^Processor board ID (.*)\s*$', show_version, flags=re.M)
+if match:
+    ser_num = match.group(1)
+
+match = re.search(r'^Configuration register is (.*)\s*$', show_version, flags=re.M)
+if match:
+    conf_reg = match.group(1)
+
+print('OS Version: ' +  os_ver)
+print('Serial Number: ' +  ser_num)
+print('Config Register: ' + conf_reg)
---
+++ lesson4/l4e5.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python3
+from __future__ import unicode_literals, print_function
+from pprint import pprint
+import re
+
