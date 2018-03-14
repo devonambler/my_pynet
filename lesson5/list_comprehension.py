cf790a995bb93b59a48ec36be3f320e8ee899ceb
devonambler <devon.ambler@gmail.com>
2018-03-14 09:19:43 -0700

    lesson5

---
+++ lesson5/list_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+my_list = [1, 2, 3, 4, 6, 6]
+
+print(my_list)
+
+my_list2 = [x**2 for x in my_list]
+
+print(my_list2)
---
+++ lesson5/my_lambda.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+def my_func(x):
+    return x**2
+
+print(my_func(10))
+
+f = lambda x: x**2
+
+print(f(10))
